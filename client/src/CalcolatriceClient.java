df91ac3c7116295a54876f9cc9c46b25cf4be705
Test <test@test.com>
2020-10-18 20:46:23 +0200

    'client-server'

--- client/src/CalcolatriceClient.java
+++ client/src/CalcolatriceClient.java
@@ -4,8 +4,75 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class CalcolatriceClient {
+public class CalcolatriceClient 
+{
 
-	
-	
+	String ip_server="127.0.0.1";
+    int porta_server=8888;
+    Socket socket;
+    BufferedReader keyboard_input;
+    String message;
+    String answer;
+    DataOutputStream sever_input;
+    BufferedReader server_output;
+    
+    public Socket connection ()
+    {
+        System.out.println("Inizio dell'esecuzione del client.");
+        try
+        {
+            keyboard_input=new BufferedReader(new InputStreamReader(System.in));
+            socket=new Socket(ip_server,porta_server);
+            sever_input=new DataOutputStream(socket.getOutputStream());
+            server_output=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Connessione al server riuscita.");
+        }
+        catch(UnknownHostException e)
+        {
+        	System.out.println(e.getMessage());
+            System.err.println("Host sconosciuto.");
+        }
+        catch (Exception e) 
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Connessione non riuscita.");
+            System.exit(1);
+        }
+        return(socket);
+    }
+    
+    public void send ()
+    {
+    	for (;;)
+    	{
+    		try 
+    		{
+                System.out.println("Inserire primo numero");
+                message=keyboard_input.readLine();
+                System.out.println("Invio dati al server.");
+                sever_input.writeBytes(message+'\n');
+                answer=server_output.readLine();
+                System.out.println("Inserire operazione desiderata");
+                message=keyboard_input.readLine();
+                System.out.println("Invio dati al server.");
+                sever_input.writeBytes(message+'\n');
+                answer=server_output.readLine();
+                System.out.println("Inserire secondo numero");
+                message=keyboard_input.readLine();
+                System.out.println("Invio dati al server.");
+                sever_input.writeBytes(message+'\n');
+                answer=server_output.readLine();
+                answer=server_output.readLine();
+                System.out.println("Risposta da parte del server: "+answer);
+                System.out.println("Chiusura connessione.");
+                socket.close();
+            }
+            catch (Exception e) 
+    		{
+                System.out.println(e.getMessage());
+                System.out.println("Errore durante l'invio al server.");
+                System.exit(1);
+            }
+    	}
+    }
 }
--- server/src/CalcolatriceServer.java
+++ server/src/CalcolatriceServer.java
@@ -4,8 +4,74 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class CalcolatriceServer {
+public class CalcolatriceServer 
+{
 
-	
-	
+	Socket client_socket=null;
+	ServerSocket server_socket=null;
+    int client_number1=0;
+    String client_symbol="";
+    int client_number2=0;
+    String server_answer=null;
+    BufferedReader client_input;
+    DataOutputStream client_output;
+    
+    public Socket waitForClient ()
+    {
+        try 
+        {
+            System.out.println("Inizio esecuzione server.");
+            server_socket=new ServerSocket(8888);
+            System.out.println("Attesa di connessione da parte del client.");
+            client_socket=server_socket.accept();
+            System.out.println("Connessione del client ricevuta.");
+            client_input=new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+            client_output=new DataOutputStream(client_socket.getOutputStream());
+        }
+        catch (Exception e) 
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'esecuzione del server.");
+            System.exit(1);
+        }
+        return(client_socket);
+    }
+    
+    public void calculate ()
+    {
+    		try 
+            {
+                System.out.println("Attesa del primo numero in input dal client.");
+                client_number1=Integer.valueOf(client_input.readLine());
+                System.out.println("Primo numero ricevuto.");
+                client_output.writeBytes("OK"+'\n');
+                System.out.println("Attesa del simbolo dell'operazione in input dal client.");
+                client_symbol=client_input.readLine();
+                System.out.println("Simbolo ricevuto.");
+                client_output.writeBytes("OK"+'\n');
+                System.out.println("Attesa del secondo numero in input dal client.");
+                client_number2=Integer.valueOf(client_input.readLine());
+                System.out.println("Secondo numero ricevuto.");
+                client_output.writeBytes("OK"+'\n');
+                switch (client_symbol)
+                {
+                case "+":
+                	client_output.writeBytes(String.valueOf(client_number1+client_number2)+'\n');
+                case "-":
+                	client_output.writeBytes(String.valueOf(client_number1-client_number2)+'\n');
+                case "*":
+                	client_output.writeBytes(String.valueOf(client_number1*client_number2)+'\n');
+                case "/":
+                	client_output.writeBytes(String.valueOf(client_number1/client_number2)+'\n');
+                case "": 
+                	throw new Exception("Simbolo non valido.");
+                }
+                System.out.println("Operazione completata.");
+                client_socket.close();
+            }
+            catch (Exception e) 
+            {
+                System.out.println("...");
+            }
+    }
 }
